d8b40b014132478552d078ebbe4e67a75356d41e
Scott Neibarger <scott.neibarger@gmail.com>
2025-12-08 14:09:37 -0500

    Initial commit.

---
+++ src/main/java/org/springy/springy_avro/SpringyAvroApplication.java
@@ -0,0 +1,13 @@
+package org.springy.springy_avro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringyAvroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringyAvroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springy/springy_avro/SpringyAvroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springy.springy_avro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringyAvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
